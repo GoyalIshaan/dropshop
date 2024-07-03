b329c36648d36566fcaab04287d52213bc7d7b1e
GoyalIshaan <goyalishaan2005@gmail.com>
2024-07-03 14:54:50 +0530

    tsconfigs

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+declare module '@vitejs/plugin-react-swc';
+declare module 'react-slick';
+declare module 'react-helmet';
+declare module 'jsonwebtoken';
+declare module 'bcryptjs';
+declare module 'cookie-parser';
