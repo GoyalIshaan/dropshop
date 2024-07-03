88622feb30f0edfbe5c002d00a9abbddb6b9eca7
GoyalIshaan <goyalishaan2005@gmail.com>
2024-07-03 17:00:49 +0530

    chnaged

--- src/db.ts
+++ src/db.ts
@@ -4,10 +4,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     // establishes the connection to the database
-    const conn = await mongoose.connect(process.env.MONGO_URI!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: unknown) {
     if (error instanceof Error) {
